40501457c512f4599d08430eb0792a0ebc42fa7c
domib22 <domib22@users.noreply.github.com>
2020-03-21 17:33:59 +0100

    embedded jetty; add web.xml

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -9,15 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "Hello", urlPatterns = {"/api/*"})
 public class HelloServlet extends HttpServlet {
+    private static final String NAME_PARAM = "name";
+
     private final Logger log = LoggerFactory.getLogger(HelloServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.info("Request got");
+        log.info("Got request with parameters " + req.getParameterMap());
+        String name = Optional.ofNullable(req.getParameter(NAME_PARAM)).orElse("stranger");
 
-        resp.getWriter().write("Hello!");
+        resp.getWriter().write("Hello " + name + "!");
     }
 }
